fcb310ce44879f1648a95fb20527a4318e0d29de
ItsMAgames <moralesdamudio@gmail.com>
2022-12-07 18:50:19 -0600

    Temporizador nuevo

---
+++ src/timer.lua
@@ -0,0 +1,28 @@
+--CREACION DE UN TEMPORIZADOR
+
+--VARIABLE REQUERIDA
+local clock = os.clock
+
+--FUNCION PARA CREAR UN TEMPORIZADOR
+function Sleep(n) --n : seconds
+
+    --VARIABLE ESPECIAL
+    
+    local t0 = clock()
+    
+    --local resultado = clock() - t0
+    
+    while clock() - t0 <= n do 
+        Terminado = false
+
+        if clock() - t0 <= n then
+            Terminado = true
+        end
+    end
+
+    if Terminado == true then
+        print("DEBUG: Tiempo terminado")
+    end
+end
+
+--Sleep(io.read("n"))
